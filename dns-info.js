cfcffe45f718d62680c3ef9640643f5567bedd3e
NerdyTechy <techy@techy.lol>
2022-12-27 19:13:23 +0000

    Simplified code, formatted with prettier

--- dns-info.js
+++ dns-info.js
@@ -1,57 +1,57 @@
-var dns = require('native-dns')
-var Promise = require('bluebird')
-var _ = require('lodash')
-var defaults = {
-  server: {
-    address: '8.8.8.8',
-    port: 53,
-    type: 'udp'
-  },
-  timeout: 2000
-}
+var dns = require("native-dns");
+var Promise = require("bluebird");
+var _ = require("lodash");
+
+const defaults = {
+    server: {
+        address: "8.8.8.8",
+        port: 53,
+        type: "udp",
+    },
+    timeout: 2000,
+};
 
 module.exports = function fetchDnsInfo(param) {
-  var config = parseConfig(param, defaults)
-  var allTypes = Object.keys(dns.consts.NAME_TO_QTYPE).map(function(type) {
-    return new Promise(function(resolve, reject) {
-      dns.Request({
-        question: dns.Question({
-          name: config.domain,
-          type: type
-        }),
-        server: config.server,
-        timeout: config.timeout
-      }).on('timeout', function() {
-        reject('Timeout in fetching DNS info.')
-      }).on('message', function(err, res) {
-        resolve(err || res)
-      }).send()
-    })
-  })
-  return Promise.reduce(allTypes, function(result, response) {
-    if (response.answer.length) {
-      var ANY = 255
-      if (response.question[0].type === ANY) {
-        result.byAny = response.answer.map(function(entry) {
-          entry.type = dns.consts.QTYPE_TO_NAME[entry.type]
-          return _(entry).omit('name', 'class').value()
-        })
-      } else {
-        var questionType = dns.consts.QTYPE_TO_NAME[response.question[0].type]
-        var data = response.answer.map(function(entry) {
-          return _(entry).omit('type', 'name', 'class').value()
-        })
-        result.byTypes.push({type: questionType, data: data})
-      }
-    }
-    return result
-  }, {domain: config.domain, byTypes: [], byAny: undefined})
-}
+    var config = parseConfig(param, defaults);
+    var allTypes = Object.keys(dns.consts.NAME_TO_QTYPE).map(function (type) {
+        return new Promise(function (resolve, reject) {
+            dns.Request({
+                question: dns.Question({
+                    name: config.domain,
+                    type: type,
+                }),
+                server: config.server,
+                timeout: config.timeout,
+            })
+                .on("timeout", function () {
+                    reject("Timeout in fetching DNS info.");
+                })
+                .on("message", function (err, res) {
+                    resolve(err || res);
+                })
+                .send();
+        });
+    });
+    return Promise.reduce(
+        allTypes,
+        function (result, response) {
+            if (response.answer.length) {
+                var questionType = dns.consts.QTYPE_TO_NAME[response.question[0].type];
+                var data = response.answer.map(function (entry) {
+                    return _(entry).omit("type", "name", "class").value();
+                });
+                result.records.push({ type: questionType, data: data });
+            }
+            return result;
+        },
+        { domain: config.domain, records: [] }
+    );
+};
 
 function parseConfig(param, defaults) {
-  if (typeof param === 'string') {
-    return _.assign({}, defaults, {domain: param})
-  } else {
-    return _.assign({}, defaults, param)
-  }
+    if (typeof param === "string") {
+        return _.assign({}, defaults, { domain: param });
+    } else {
+        return _.assign({}, defaults, param);
+    }
 }
